69cc50a93ab754f4b6248db8ae37bdbfa097a213
Crowdbotics <team@crowdbotics.com>
2023-04-14 00:39:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/intuitive_39917/MainActivity.java
@@ -0,0 +1,15 @@
+package com.intuitive_39917;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "intuitive_39917";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "intuitive-39917",
+  url: "https://intuitive-39917.botics.co",
+  api: "https://intuitive-39917.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
